80bd3e12209fd8ac5faf8b8420f4b987a8251322
Lai-khan <40736969+Lai-khan@users.noreply.github.com>
2023-02-21 22:10:28 +0900

    메인화면, 팀수화면

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './components/Main';
 import TeamNum from './components/TeamNum';
 import GameBoard from './components/GameBoard';
@@ -9,8 +9,8 @@ const App = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Main />} />
-			<Route path='/' element={<TeamNum />} />
-			<Route path='/' element={<GameBoard />} />
+			<Route path='/num' element={<TeamNum />} />
+			<Route path='/game' element={<GameBoard />} />
 		</Routes>
 	);
 };

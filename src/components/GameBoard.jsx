fe955405e3fe2e4da164afe926ff11a8ad4641ff
Lai-khan <40736969+Lai-khan@users.noreply.github.com>
2023-02-24 00:11:53 +0900

    게임화면

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import Grid from '@mui/material/Grid';
+
+import '../style/GameBoard.css';
 
 const GameBoard = () => {
-	return <></>;
+	const teamNum = 6;
+	const height = 900 / teamNum;
+
+	return (
+		<div id='game'>
+			<div id='teamMarker'>
+				<Grid container direction='column' justifyContent='space-between' alignItems='stretch'>
+					<Grid item className='team' style={{ height: height }}></Grid>
+					<Grid item className='team' style={{ height: height }}></Grid>
+					<Grid item className='team' style={{ height: height }}></Grid>
+					<Grid item className='team' style={{ height: height }}></Grid>
+					<Grid item className='team' style={{ height: height }}></Grid>
+					<Grid item className='team' style={{ height: height }}></Grid>
+				</Grid>
+			</div>
+			<div id='gameBoard'></div>
+		</div>
+	);
 };
 
 export default GameBoard;
